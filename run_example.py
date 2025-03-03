13781411bb05d773ea101f68e778bd48b92f00ab
Michal Masrna <m.masrna1@gmail.com>
2025-03-03 10:42:47 +0100

    changed handling of tmp files for better debugging

--- run_example.py
+++ run_example.py
@@ -16,11 +16,10 @@ os.makedirs("example/tmp_data/", exist_ok=True)
 if os.system("arm-none-eabi-gcc -Wall -Wpedantic -std=c11 -ffreestanding -nostdlib -mthumb -march=armv7-m -O0 -Wl,-Texample/linker.ld -Wl,-Map,example/tmp_data/binary.map example/example.c -o example/tmp_data/binary.elf") != 0: sys.exit(1)
 
 # create disassembly for looking up armory results later
-if os.system("arm-none-eabi-objdump example/tmp_data/binary.elf -d > disassembled.txt") != 0: sys.exit(1)
+if os.system("arm-none-eabi-objdump example/tmp_data/binary.elf -d > example/tmp_data/disassembled.txt") != 0: sys.exit(1)
 
 # start fault simulation
 if os.system("cd example && python3 start_emulation.py") != 0: sys.exit(1)
 
 # cleanup tmp data
-shutil.rmtree("example/tmp_data/")
-
+# shutil.rmtree("example/tmp_data/")
